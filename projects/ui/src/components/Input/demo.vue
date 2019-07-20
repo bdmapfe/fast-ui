ea30155eb1f74749f79feb0529b5348f2760dcc8
huangwenming <1205597228@qq.com>
2019-07-20 18:23:02 +0800

    ui:add the input com

---
+++ projects/ui/src/components/Input/demo.vue
@@ -0,0 +1,89 @@
+<template>
+    <div class="demo-wrapper">
+        <h4>基本用法</h4>
+        <p class="desc">input-basic</p>
+        <div class="test-box">
+            <fast-input label="车牌号码：" placeholder="请输入车牌号"></fast-input>
+        </div>
+
+        <p class="desc">input-no-border</p>
+        <div class="test-box">
+            <fast-input label="车牌号码：" :has-border="false" placeholder="请输入车牌号"></fast-input>
+        </div>
+
+        <p class="desc">input-disabled</p>
+        <div class="test-box">
+            <fast-input label="车牌号码：" disabled="true" placeholder="请输入车牌号"></fast-input>
+        </div>
+
+        <p class="desc">input-readonly</p>
+        <div class="test-box">
+            <fast-input label="车牌号码：" value="京A34569" readonly="true" placeholder="请输入车牌号"></fast-input>
+        </div>
+
+        <p class="desc">input-maxlength</p>
+        <div class="test-box">
+            <fast-input label="车牌号码：" maxlength="8" placeholder="车牌号长度不超过8位"></fast-input>
+        </div>
+
+        <p class="desc">input-backgroundColor</p>
+        <div class="test-box">
+            <fast-input label="车牌号码：" :has-bg-color="true" placeholder="车牌号长度不超过8位"></fast-input>
+        </div>
+
+        <p class="desc">input-noclear</p>
+        <div class="test-box">
+            <fast-input label="车牌号码：" :clear-btn-exist="false" placeholder="车牌号长度不超过8位"></fast-input>
+        </div>
+
+        <p class="desc">input-events</p>
+        <div class="test-box">
+            <fast-input label="车牌号码：" @input="inputChange" placeholder="车牌号长度不超过8位"></fast-input>
+        </div>
+    </div>
+</template>
+
+<script>
+    import Input from './Input.vue'
+    export default {
+        name: "Input.demo",
+        components: {
+            'fast-input': Input
+        },
+        methods: {
+            inputChange(value) {
+                alert('event-input-emit:: the value of the input is: ' + value);
+            }
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+    .demo-wrapper {
+        padding: 30px;
+    }
+    .desc {
+        color: #999;
+        padding-bottom: 16px;
+        font-size: 24px;
+    }
+    .test-box {
+        background: #fff;
+        margin-bottom: 40px;
+        padding: 30px;
+        font-size: 24px;
+    }
+    .taller {
+        height: 100px;
+    }
+    .test-item {
+        height: 50px;
+        line-height: 50px;
+        color: #fff;
+        text-align: center;
+        background: #3385ff;
+    }
+    .light {
+        opacity: .6;
+    }
+</style>
