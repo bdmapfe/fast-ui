d5012973a1f80067067d3788037b4c8b802496e5
Zxy-Code <376698256@qq.com>
2019-07-20 18:46:07 +0800

    demos项目增加点击toast

--- projects/demos/src/pages/home/main.js
+++ projects/demos/src/pages/home/main.js
@@ -2,6 +2,8 @@ import Vue from 'vue';
 
 import App from './app.vue';
 import router from './router';
+import Toast from '@ui/src/plugins/Toast/index';
+Vue.use(Toast);
 
 import lib from '@/libs/js/flexible.debug.js';
 import fastUI from '@ui/src/index.js';
--- projects/ui/src/components/Cell/demo.vue
+++ projects/ui/src/components/Cell/demo.vue
@@ -4,8 +4,8 @@
         <div class="base">
             <Cell title="这是标题" desc="描述性文字"></Cell>
             <Cell title="点击跳转到百度" :isLink="true" linkUrl="http://www.baidu.com" :showArrow="true"></Cell>
-            <Cell title="这是标题" :showArrow="false" desc="右侧是一个默认的可点控件" :controlConfig="{borderRadius: '20%'}"></Cell>
-            <Cell title="这是标题" :showArrow="false" desc="右侧是一个自定义的可点控件" :controlConfig="{img:'https://himg.bdimg.com/sys/portrait/item/fc89c885.jpg?time=6296', borderRadius: '20%'}"></Cell>
+            <Cell title="这是标题" :showArrow="false" desc="右侧是一个默认的可点控件" :controlConfig="{borderRadius: '20%'}" @click-right-control="clickRightControl"></Cell>
+            <Cell title="这是标题" :showArrow="false" desc="右侧是一个自定义的可点控件" :controlConfig="{img:'https://himg.bdimg.com/sys/portrait/item/fc89c885.jpg?time=6296', borderRadius: '20%'}" @click-right-control="clickRightControl"></Cell>
             <Cell title="这是标题" subTitle="这是副标题" desc="描述性文字" :showArrow="true"></Cell>
             <Cell cover="https://himg.bdimg.com/sys/portrait/item/fc89c885.jpg?time=6296" title="纸糖人" subTitle="专注书评写作，推荐各种好书"  desc="描述性文字"></Cell>
         </div>
@@ -33,6 +33,13 @@
 
         components: {
             Cell
+        },
+
+        methods: {
+            // 点击右侧控件
+            clickRightControl() {
+                this.$toast('You click Me', {duration: 2000});
+            }
         }
     }
 </script>
