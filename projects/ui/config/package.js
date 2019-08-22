9c29d4b172c6087c1a064b288312363cf993d3c1
fuqiaochu <fuqiaochu@baidu.com>
2019-08-22 19:20:18 +0800

    进度条

--- projects/ui/config/package.js
+++ projects/ui/config/package.js
@@ -1,226 +1,228 @@
 const uiPackage = {
-  categories: [
-      {
-          name: '布局组件',
-          nameEn: 'layout',
-          list: [
-              {
-                  name: 'Flex',
-                  version: '1.0.0',
-                  desc: 'Flex布局',
-                  type: 'component',
-                  author: 'hwm',
-                  hasDemo: true,
-                  hasDoc: true
-              },
-              {
-                  name: 'Cell',
-                  version: '1.0.0',
-                  desc: '列表项',
-                  type: 'component',
-                  author: 'zxy',
-                  hasDemo: true,
-                  hasDoc: true
-              }
-          ]
-      },
-      {
-          name: '基础组件',
-          nameEn: 'basic',
-          list: [
-              {
-                  name: 'Notice',
-                  version: '1.0.0',
-                  desc: '小黄条通知',
-                  type: 'component',
-                  author: 'hwm',
-                  hasDemo: true,
-                  hasDoc: true
-              },
-              {
-                  name: 'Select',
-                  version: '1.0.0',
-                  desc: '选择器',
-                  type: 'component',
-                  author: 'hwm',
-                  hasDemo: true,
-                  hasDoc: true
-              },
-              {
-                  name: 'Toast',
-                  version: '1.0.0',
-                  desc: 'toast',
-                  type: 'plugin',
-                  author: 'hwm',
-                  hasDemo: true,
-                  hasDoc: true
-              },
-              {
-                  name: 'Dialog',
-                  version: '1.0.0',
-                  desc: '弹窗',
-                  type: 'plugin',
-                  author: 'hwm',
-                  hasDemo: true,
-                  hasDoc: true
-              },
-              {
-                  name: 'Button',
-                  version: '1.0.0',
-                  desc: '按钮',
-                  type: 'component',
-                  author: 'yst',
-                  hasDemo: true,
-                  hasDoc: true
-              },
-              {
-                  name: 'Icon',
-                  version: '1.0.0',
-                  desc: '图标',
-                  type: 'component',
-                  author: 'yc',
-                  hasDemo: true,
-                  hasDoc: true
-              },
-              {
-                  name: 'Radio',
-                  version: '1.0.0',
-                  desc: '单选框',
-                  type: 'component',
-                  author: 'lb',
-                  hasDemo: true,
-                  hasDoc: true
-              },
-              {
-                name: 'CheckBox',
+    categories: [{
+            name: '布局组件',
+            nameEn: 'layout',
+            list: [{
+                    name: 'Flex',
+                    version: '1.0.0',
+                    desc: 'Flex布局',
+                    type: 'component',
+                    author: 'hwm',
+                    hasDemo: true,
+                    hasDoc: true
+                },
+                {
+                    name: 'Cell',
+                    version: '1.0.0',
+                    desc: '列表项',
+                    type: 'component',
+                    author: 'zxy',
+                    hasDemo: true,
+                    hasDoc: true
+                }
+            ]
+        },
+        {
+            name: '基础组件',
+            nameEn: 'basic',
+            list: [{
+                    name: 'Notice',
+                    version: '1.0.0',
+                    desc: '小黄条通知',
+                    type: 'component',
+                    author: 'hwm',
+                    hasDemo: true,
+                    hasDoc: true
+                },
+                {
+                    name: 'Select',
+                    version: '1.0.0',
+                    desc: '选择器',
+                    type: 'component',
+                    author: 'hwm',
+                    hasDemo: true,
+                    hasDoc: true
+                },
+                {
+                    name: 'Toast',
+                    version: '1.0.0',
+                    desc: 'toast',
+                    type: 'plugin',
+                    author: 'hwm',
+                    hasDemo: true,
+                    hasDoc: true
+                },
+                {
+                    name: 'Dialog',
+                    version: '1.0.0',
+                    desc: '弹窗',
+                    type: 'plugin',
+                    author: 'hwm',
+                    hasDemo: true,
+                    hasDoc: true
+                },
+                {
+                    name: 'Button',
+                    version: '1.0.0',
+                    desc: '按钮',
+                    type: 'component',
+                    author: 'yst',
+                    hasDemo: true,
+                    hasDoc: true
+                },
+                {
+                    name: 'Icon',
+                    version: '1.0.0',
+                    desc: '图标',
+                    type: 'component',
+                    author: 'yc',
+                    hasDemo: true,
+                    hasDoc: true
+                },
+                {
+                    name: 'Radio',
+                    version: '1.0.0',
+                    desc: '单选框',
+                    type: 'component',
+                    author: 'lb',
+                    hasDemo: true,
+                    hasDoc: true
+                },
+                {
+                    name: 'CheckBox',
+                    version: '1.0.0',
+                    desc: '复选框',
+                    type: 'component',
+                    author: 'lb',
+                    hasDemo: true,
+                    hasDoc: true
+                },
+                {
+                    name: 'Switch',
+                    version: '1.0.0',
+                    desc: '开关',
+                    type: 'component',
+                    author: 'lb',
+                    hasDemo: true,
+                    hasDoc: true
+                },
+                {
+                    name: 'Rate',
+                    version: '1.0.0',
+                    desc: '评分',
+                    type: 'component',
+                    author: 'cc',
+                    hasDemo: true,
+                    hasDoc: true
+                },
+                {
+                    name: 'ProgressBar',
+                    version: '1.0.0',
+                    desc: '进度条',
+                    type: 'component',
+                    author: 'fqc',
+                    hasDemo: true,
+                    hasDoc: true
+                }
+            ]
+        },
+        {
+            name: '表单组件',
+            nameEn: 'form',
+            list: [{
+                    name: 'Input',
+                    version: '1.0.0',
+                    desc: '文本输入框',
+                    type: 'component',
+                    author: 'hwm',
+                    hasDemo: true,
+                    hasDoc: true
+                },
+                {
+                    name: 'ActionSheet',
+                    version: '1.0.0',
+                    desc: '动作面板',
+                    type: 'component',
+                    author: 'hwm',
+                    hasDemo: true,
+                    hasDoc: true
+                }
+            ]
+        },
+        {
+            name: '数据展示',
+            nameEn: 'dataDisplay',
+            list: [{
+                    name: 'Swiper',
+                    version: '1.0.0',
+                    desc: 'h5-swiper',
+                    type: 'component',
+                    author: 'hwm',
+                    hasDemo: true
+                },
+                {
+                    name: 'changesvgcolor',
+                    version: '1.0.0',
+                    desc: 'svg素材更换颜色',
+                    type: 'directive',
+                    author: 'hwm',
+                    hasDemo: true
+                },
+                {
+                    name: 'linechat',
+                    version: '1.0.0',
+                    desc: '折线图组件',
+                    type: 'directive',
+                    author: 'cc',
+                    hasDemo: true
+                },
+                {
+                    name: 'Skeleton',
+                    version: '1.0.0',
+                    desc: '骨架图',
+                    type: 'component',
+                    author: 'zxy',
+                    hasDemo: true
+                },
+                {
+                    name: 'Accordion',
+                    version: '1.0.0',
+                    desc: '手风琴',
+                    type: 'component',
+                    author: 'zxy',
+                    hasDemo: true
+                },
+                {
+                    name: 'CountDown',
+                    version: '1.0.0',
+                    desc: '倒计时',
+                    type: 'component',
+                    author: 'yst',
+                    hasDemo: true,
+                    hasDoc: true
+                },
+                {
+                    name: 'Scroller',
+                    version: '1.0.0',
+                    desc: '滚动',
+                    type: 'component',
+                    author: 'luoqin',
+                    hasDemo: true
+                }
+            ]
+        },
+        {
+            name: '业务组件',
+            nameEn: 'CarPlatePrefixer',
+            list: [{
+                name: 'CarPlatePrefixer',
                 version: '1.0.0',
-                desc: '复选框',
+                desc: '车牌归属地选择面板',
                 type: 'component',
-                author: 'lb',
-                hasDemo: true,
-                hasDoc: true
-            },
-            {
-                name: 'Switch',
-                version: '1.0.0',
-                desc: '开关',
-                type: 'component',
-                author: 'lb',
-                hasDemo: true,
-                hasDoc: true
-            },
-            {
-                  name: 'Rate',
-                  version: '1.0.0',
-                  desc: '评分',
-                  type: 'component',
-                  author: 'cc',
-                  hasDemo: true,
-                  hasDoc: true
-            }
-          ]
-      },
-      {
-          name: '表单组件',
-          nameEn: 'form',
-          list: [
-              {
-                  name: 'Input',
-                  version: '1.0.0',
-                  desc: '文本输入框',
-                  type: 'component',
-                  author: 'hwm',
-                  hasDemo: true,
-                  hasDoc: true
-              },
-              {
-                  name: 'ActionSheet',
-                  version: '1.0.0',
-                  desc: '动作面板',
-                  type: 'component',
-                  author: 'hwm',
-                  hasDemo: true,
-                  hasDoc: true
-              }
-          ]
-      },
-      {
-          name: '数据展示',
-          nameEn: 'dataDisplay',
-          list: [
-              {
-                  name: 'Swiper',
-                  version: '1.0.0',
-                  desc: 'h5-swiper',
-                  type: 'component',
-                  author: 'hwm',
-                  hasDemo: true
-              },
-              {
-                  name: 'changesvgcolor',
-                  version: '1.0.0',
-                  desc: 'svg素材更换颜色',
-                  type: 'directive',
-                  author: 'hwm',
-                  hasDemo: true
-              },
-              {
-                  name: 'linechat',
-                  version: '1.0.0',
-                  desc: '折线图组件',
-                  type: 'directive',
-                  author: 'cc',
-                  hasDemo: true
-              },
-              {
-                  name: 'Skeleton',
-                  version: '1.0.0',
-                  desc: '骨架图',
-                  type: 'component',
-                  author: 'zxy',
-                  hasDemo: true
-              },
-              {
-                  name: 'Accordion',
-                  version: '1.0.0',
-                  desc: '手风琴',
-                  type: 'component',
-                  author: 'zxy',
-                  hasDemo: true
-              },
-              {
-                  name: 'CountDown',
-                  version: '1.0.0',
-                  desc: '倒计时',
-                  type: 'component',
-                  author: 'yst',
-                  hasDemo: true,
-                  hasDoc: true
-              },
-              {
-                  name: 'Scroller',
-                  version: '1.0.0',
-                  desc: '滚动',
-                  type: 'component',
-                  author: 'luoqin',
-                  hasDemo: true
-              }
-          ]
-      },
-      {
-          name: '业务组件',
-          nameEn: 'CarPlatePrefixer',
-          list: [
-              {
-                  name: 'CarPlatePrefixer',
-                  version: '1.0.0',
-                  desc: '车牌归属地选择面板',
-                  type: 'component',
-                  author: 'hwm',
-                  hasDemo: true
-              }
-          ]
-      },
-  ]
+                author: 'hwm',
+                hasDemo: true
+            }]
+        },
+    ]
 };
 export default uiPackage;
--- projects/ui/src/components/ProgressBar/README.md
+++ projects/ui/src/components/ProgressBar/README.md
@@ -3,22 +3,27 @@
 
 ## 基本用法
 
-通过isShow控制显隐，通过data传入数据
+向percentage参数传入进度百分比，例如百分之30传入"30"
 
 ```html
-    <notice :isShow="showNotice" :data="noticeData" @notice-click="clickNotice" @close-notice="closeNotice"></notice>
+    <progress-bar percentage="30"/>
 ```
 
 ## Props
 
 | 字段 | 说明 | 类型 | 默认值
 |----- | ----- | ----- | ----- 
-| isShow | 是否显示通知条| Boolean | false
+| percentage | 进度百分比 | string | "0" |
+| showText | 是否在末尾展示数字百分比 | bool | true |
+| textInside | 是否将数字百分比展示在进度条内 | bool | false |
+| strokeColor | 进度条颜色 | string | ”#f76454“ |
+| strokeWidth | 进度条高度 | string | ”20“ |
 
 
-## Events
+
+## Events 待补充
 
 | 字段 | 说明 | 回调参数 
 |----- | ----- | ----- 
-| notice-click | 点击通知条内容事件 | -- 
+| | | -- 
 
